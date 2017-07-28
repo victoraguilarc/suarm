c5fad4113f7ad46dac45f0a7ac9f9e2fb762917b
Victor Aguilar C <victoraguilar.net@gmail.com>
2017-07-28 01:04:31 -0400

    update docs

--- suarm/cli.py
+++ suarm/cli.py
@@ -2,17 +2,17 @@
 from .cluster import *
 
 @click.group(chain=True)
+@click.option('--config', '-f', type=click.Path(), help='Config file "swarm.json"')
 def main():
     click.echo("\nStarting...")
 
 
 @main.command('node')
-@click.option('--config', '-f', type=click.Path(), help='Config file "swarm.json"')
 @click.option('--resize', '-r', is_flag=True, help='Upgrade plan of a node')
 @click.option('--delete', '-d', is_flag=True, help='Delete a node')
-@click.option('--plan', '-p', type=int, default=None, help='PlanID of the node')
-@click.option('--subid', '-s', type=str, default=None, help='SubID of the node')
-def node(config, resize, delete, plan, subid):
+@click.option('--plan', '-p', type=int, default=None, help='Resize a node')
+@click.option('--subid', '-s', type=str, default=None, help='SUbID for node operations')
+def node(resize, delete, plan, subid):
     if resize:
         click.echo('-- ON Resize...')
         if plan and subid:
@@ -30,11 +30,10 @@ def node(config, resize, delete, plan, subid):
 
 
 @main.command('keys')
-@click.option('--config', '-f', type=click.Path(), help='Config file "swarm.json"')
 @click.option('--create', '-g', is_flag=True, help='Generate and register an sshkey')
 @click.option('--show', '-l', is_flag=True, help='List your sshkeys')
-@click.option('--delete', '-l', is_flag=True, help='List your sshkeys')
-def keys(config, create, show, delete):
+@click.option('--delete', '-d', is_flag=True, help='Delete ssh-key')
+def keys(create, show, delete):
     if create:
         register_sshkey()
     elif show:
@@ -47,11 +46,10 @@ def keys(config, create, show, delete):
 
 
 @main.command('cluster')
-@click.option('--config', '-f', type=click.Path(), help='Config file "swarm.json"')
-@click.option('--create', '-c', is_flag=True, help='Create option')
-@click.option('--delete', '-d', is_flag=True, help='Delete Option')
-@click.option('--add-node', '-a', type=int, default=None, help='Delete Option')
-def cluster(config, create, delete, add_node):
+@click.option('--create', '-c', is_flag=True, help='Create a Cluster based on swarm.json')
+@click.option('--delete', '-d', is_flag=True, help='Delete a Current cluster')
+@click.option('--add-node', '-a', type=int, default=None, help='Add worker to cluster')
+def cluster(create, delete, add_node):
     if create:
         create_cluster()
     elif delete:
@@ -64,8 +62,8 @@ def cluster(config, create, delete, add_node):
 
 @main.command('set')
 @click.option('--service', type=click.Choice(['dashboard', 'manager', 'worker']))
-@click.option('--node', '-n', type=str, help='NodeID on Vultr')
-def set(service, node):
+@click.option('--subid', '-n', type=str, help='NodeID on Vultr')
+def set(service, subid):
     if service == 'dashboard':
         pass
     elif service == 'manager':
@@ -76,12 +74,10 @@ def set(service, node):
 
 
 @main.command('loadbalancer')
-@click.option('--config', '-f', type=click.Path(), help='Add load balancer')
-@click.option('--create', '-c', is_flag=True, help='Delete or not')
-@click.option('--delete', '-d', is_flag=True, help='Delete or not')
-@click.option('--setup', '-s', is_flag=True, help='HTTPS support')
-def loadbalancer(config, create, delete, setup):
-    click.echo('--> Load balancer...')
+@click.option('--create', '-c', is_flag=True, help='Create the load balancer')
+@click.option('--delete', '-d', is_flag=True, help='Delete the load balancer')
+@click.option('--setup', '-s', is_flag=True, help='Configure the load balancer')
+def loadbalancer(create, delete, setup):
     if create:
         add_loadbalancer()
     elif setup:
@@ -91,10 +87,9 @@ def loadbalancer(config, create, delete, setup):
 
 
 @main.command('app')
-@click.option('--config', '-f', type=click.Path(), help='Add load balancer')
-@click.option('--create', '-c', is_flag=True, help='Create option')
-@click.option('--delete', '-d', is_flag=True, help='Delete Option')
-def app(config, create, delete):
+@click.option('--create', '-c', is_flag=True, help='Register an app for deploy in the cluster')
+@click.option('--delete', '-d', is_flag=True, help='Delete an app from de cluster')
+def app(create, delete):
     click.echo('--> Load balancer...')
     if create:
         click.echo('--> CREATE')
